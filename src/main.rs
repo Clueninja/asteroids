f3f57f35218af00395a27270da730813afb13142
Sam Brown <samsterbrownie@gmail.com>
2023-09-03 14:38:46 +0100

    attach camera to player, wip

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use bevy::input::keyboard::KeyboardInput;
 use bevy::sprite::Material2d;
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle, math::Vec3Swizzles};
-
+use bevy::{math::Vec3Swizzles, prelude::*, sprite::MaterialMesh2dBundle};
 
 #[derive(Component)]
 struct Velocity(Vec2);
-
 
 #[derive(Component)]
 struct Health(f32);
@@ -19,24 +17,29 @@ struct Bullet;
 #[derive(Component)]
 struct Timeout(f32);
 
-
 #[derive(Bundle)]
-struct PlayerBundle<M:Material2d>{
+struct PlayerBundle<M: Material2d> {
     id: PlayerID,
     health: Health,
     vel: Velocity,
     #[bundle()]
-    mat: MaterialMesh2dBundle<M>
+    mat: MaterialMesh2dBundle<M>,
 }
 
-
-
-
-fn main(){
+fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (update_transforms, cursor_position, move_player, fire_bullet, update_timeout))
+        .add_systems(
+            Update,
+            (
+                update_transforms,
+                cursor_position,
+                move_player,
+                fire_bullet,
+                update_timeout,
+            ),
+        )
         .run()
 }
 
@@ -44,17 +47,19 @@ fn main(){
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
-){
-    commands.spawn(Camera2dBundle::default());
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
 
     // Circle
-    commands.spawn((MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
-        ..default()
-    }, Velocity(Vec2 { x: 10., y: 0. })));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(-150., 0., 0.)),
+            ..default()
+        },
+        Velocity(Vec2 { x: 10., y: 0. }),
+    ));
 
     // Rectangle
     commands.spawn(SpriteBundle {
@@ -68,14 +73,17 @@ fn setup(
     });
 
     // Quad
-    commands.spawn((MaterialMesh2dBundle {
-        mesh: meshes
-            .add(shape::Quad::new(Vec2::new(50., 100.)).into())
-            .into(),
-        material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
-        transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
-        ..default()
-    }, Velocity(Vec2{x:20., y:-10.})));
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::Quad::new(Vec2::new(50., 100.)).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(Color::LIME_GREEN)),
+            transform: Transform::from_translation(Vec3::new(50., 0., 0.)),
+            ..default()
+        },
+        Velocity(Vec2 { x: 20., y: -10. }),
+    ));
 
     // Hexagon
     commands.spawn(MaterialMesh2dBundle {
@@ -84,82 +92,85 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(150., 0., 0.)),
         ..default()
     });
-    commands.spawn(PlayerBundle{
+    commands.spawn(PlayerBundle {
         mat: MaterialMesh2dBundle {
             mesh: meshes.add(shape::RegularPolygon::new(50., 3).into()).into(),
             material: materials.add(ColorMaterial::from(Color::TURQUOISE)),
             transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
             ..default()
         },
-        vel: Velocity(Vec2{x:0.,y:0.}),
+        vel: Velocity(Vec2 { x: 0., y: 0. }),
         id: PlayerID(0),
         health: Health(100.),
-    });
+    }).insert(Camera2dBundle::default());
 }
 use bevy::window::PrimaryWindow;
-
 
 // Converts the Cursor position to screen coordinates, then rotates the player to the Cursor
 // could make a seperate Component for objects that always rotate to the cursor
 // Contains multiple bugs I'm sure
 fn cursor_position(
     q_windows: Query<&Window, With<PrimaryWindow>>,
-    mut player_query: Query<&mut Transform, With<PlayerID>>
+    mut player_query: Query<&mut Transform, With<PlayerID>>,
 ) {
-    for mut player_transform in &mut player_query{
-        // get the player translation in 2D
-        let player_translation = player_transform.translation.xy();
+    let mut player_transform = player_query.single_mut();
+    // get the player translation in 2D
+    let player_translation = player_transform.translation.xy();
 
-        // Games typically only have one window (the primary window)
-        if let Some(position) = q_windows.single().cursor_position() {
-            let mut cloned = position;
-            cloned.x =  cloned.x -  q_windows.single().resolution.width()/2.;
-            cloned.y = q_windows.single().resolution.height()/2. - cloned.y;
+    // Games typically only have one window (the primary window)
+    if let Some(position) = q_windows.single().cursor_position() {
+        let mut cloned = position;
+        cloned.x = cloned.x - q_windows.single().resolution.width() / 2.;
+        cloned.y = q_windows.single().resolution.height() / 2. - cloned.y;
 
-            println!("{}, {}", cloned, player_translation);
-            let to_player = (cloned - player_translation).normalize();
+        println!("{}, {}", cloned, player_translation);
+        let to_player = (cloned).normalize();
 
-            // get the quaternion to rotate from the initial enemy facing direction to the direction
-            // facing the player
-            let rotate_to_player = Quat::from_rotation_arc(Vec3::Y, to_player.extend(0.));
-            player_transform.rotation = rotate_to_player;
-
-        } else {
-            println!("Cursor is not in the game window.");
-        }
+        // get the quaternion to rotate from the initial enemy facing direction to the direction
+        // facing the player
+        let rotate_to_player = Quat::from_rotation_arc(Vec3::Y, to_player.extend(0.));
+        player_transform.rotation = rotate_to_player;
+    } else {
+        println!("Cursor is not in the game window.");
     }
 }
 
 // If spacebar is pressed, spawn a new Entity, with Bullet, and timeout components with a circle sprite
 fn fire_bullet(
-    mut commands : Commands,
-    input: Res<Input<KeyCode>>,
-    query: Query<& Transform, With<PlayerID>>,
+    mut commands: Commands,
+    input: Res<Input<MouseButton>>,
+    query: Query<&Transform, With<PlayerID>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
-){
-    if input.pressed(KeyCode::Space){
-        commands.spawn(MaterialMesh2dBundle{
-            mesh: meshes.add(shape::Circle::new(5.).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::DARK_GRAY)),
-            transform: query.single().clone(),
-            ..default()
-        }).insert(Velocity(Vec2 { x: 0., y:1000. }))
-        .insert(Bullet)
-        .insert(Timeout(0.5));
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    if input.just_pressed(MouseButton::Left) {
+        commands
+            .spawn(MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(5.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::DARK_GRAY)),
+                transform: query.single().clone(),
+                ..default()
+            })
+            .insert(Velocity(Vec2 { x: 0., y: 1000. }))
+            .insert(Bullet)
+            .insert(Timeout(0.5));
     }
 }
 
 // increase and decrease player speed
-fn move_player(
-    input: Res<Input<KeyCode>>,
-    mut query: Query<&mut Velocity, With<PlayerID>>
-){
-    if input.pressed(KeyCode::W){
-        query.single_mut().0.y +=10.;
+fn move_player(input: Res<Input<KeyCode>>, mut query: Query<&mut Velocity, With<PlayerID>>) {
+    if input.pressed(KeyCode::W) {
+        if query.single().0.y < 200.{
+            query.single_mut().0.y += 10.;
+        }
     }
-    if input.pressed(KeyCode::S){
-        query.single_mut().0.y -=10.;
+    if input.pressed(KeyCode::S) {
+        if query.single().0.y >0.{
+            query.single_mut().0.y -= 10.;
+        }
+    }
+    if input.pressed(KeyCode::W) && input.pressed(KeyCode::S) {
+        query.single_mut().0.y = 0.;
     }
 }
 
@@ -167,23 +178,20 @@ fn move_player(
 fn update_timeout(
     mut commands: Commands,
     time: Res<Time>,
-    mut query: Query<(Entity, &mut Timeout)>
-){
-    for (e, mut timeout) in &mut query{
+    mut query: Query<(Entity, &mut Timeout)>,
+) {
+    for (e, mut timeout) in &mut query {
         timeout.0 -= time.delta_seconds();
-        if timeout.0 < 0.{
+        if timeout.0 < 0. {
             commands.entity(e).despawn();
         }
     }
 }
 
 // move transforms for entities with a Velocity Component
-fn update_transforms(
-    time: Res<Time>,
-    mut moving_object: Query<(& Velocity, &mut Transform)>
-){
-    for (vel, mut transform) in &mut moving_object{
-        let mut vec = Vec3::new(vel.0.x, vel.0.y, 0.);
+fn update_transforms(time: Res<Time>, mut moving_object: Query<(&Velocity, &mut Transform)>) {
+    for (vel, mut transform) in &mut moving_object {
+        let mut vec = vel.0.clone().extend(0.);
         vec = transform.rotation.mul_vec3(vec);
         transform.translation += vec * time.delta_seconds();
     }
