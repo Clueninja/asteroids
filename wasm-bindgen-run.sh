418b56594b35b36431cf706971dafe267f3abf13
Sam Brown <samsterbrownie@gmail.com>
2023-09-04 17:18:06 +0100

    refresh wasm

--- wasm-bindgen-run.sh
+++ wasm-bindgen-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cargo build --release --target wasm32-unknown-unknown                                               
-wasm-bindgen --out-dir ./docs --target web ./target/wasm32-unknown-unknown/release/asteriods.wasm
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --no-typescript --out-dir ./docs --target web ./target/wasm32-unknown-unknown/release/asteriods.wasm
